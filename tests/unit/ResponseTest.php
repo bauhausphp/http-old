7181e95c00b3cdad32b894676bfa195ab1482407
Felipe Martins <me@fefas.net>
2017-01-02 01:21:20 -0200

    Cover with tests all behaviors of Response

--- tests/unit/ResponseTest.php
+++ tests/unit/ResponseTest.php
@@ -7,17 +7,18 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function createReponseWithStatusCode200IfItWasNotInformed()
+    public function responseIsCreatedWithTheStatusCode200ByDefault()
     {
         $response = new Response();
 
         $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('OK', $response->getReasonPhrase());
     }
 
     /**
      * @test
      */
-    public function createNewResponseWithTheDesiredStatusCode()
+    public function createNewResponseGivenAStatusCodeAndExpectingTheRecommendedReasonPhrase()
     {
         $response = new Response();
 
@@ -25,5 +26,20 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
 
         $this->assertNotSame($response, $newResponse);
         $this->assertEquals(202, $newResponse->getStatusCode());
+        $this->assertEquals('Accepted', $newResponse->getReasonPhrase());
+    }
+
+    /**
+     * @test
+     */
+    public function createNewResponseGivenAStatusCodeAndAReasonPhrase()
+    {
+        $response = new Response();
+
+        $newResponse = $response->withStatus(202, 'Processing request');
+
+        $this->assertNotSame($response, $newResponse);
+        $this->assertEquals(202, $newResponse->getStatusCode());
+        $this->assertEquals('Processing request', $newResponse->getReasonPhrase());
     }
 }
